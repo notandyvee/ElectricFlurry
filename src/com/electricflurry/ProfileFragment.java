6ce86be04a84714743d35ae8fc055091bcac7c8c
Sean Crowder <s7crowder@gmail.com>
2013-04-08 03:07:50 -0400

    finished myProfile/profile interacting with database

--- src/com/electricflurry/ProfileFragment.java
+++ src/com/electricflurry/ProfileFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment implements ConsumeCursor{
 	// created the profile fragment just copying the mingle fragment -sean
@@ -26,7 +27,11 @@ public class ProfileFragment extends Fragment implements ConsumeCursor{
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.profile_fragment, container, false);
-		
+		userContents[0] = "Not available";
+		userContents[1] = "Not available";
+		urlContents[0] = "Not available";
+		urlContents[1] = "Not available";
+		urlContents[2] = "Not available";
 		final ElectricFlurryDatabase db = new ElectricFlurryDatabase(getActivity());
 		
 		profile_name = (TextView)view.findViewById(R.id.profile_name);
@@ -35,13 +40,15 @@ public class ProfileFragment extends Fragment implements ConsumeCursor{
 		twitter_url = (TextView) view.findViewById(R.id.twitter_url);
 		google_url = (TextView)view.findViewById(R.id.google_url);
 		
-		db.leQuery("users", new String[] {"user", "phone"}, null, null, null, null, null, null);
+		db.leQuery("users", new String[] {"user", "phone"}, null, null, null, null, null, this);
+		db.leQuery("social_urls", new String[] {"type", "url"}, null, null, null, null, null, this);
+		db.closeDbase();
 		
-		profile_name.setText("Profile name");
-		profile_phone.setText("Phone");
-		facebook_url.setText("Facebook");
-		twitter_url.setText("Twitter");
-		google_url.setText("Google");
+		profile_name.setText(userContents[0]);
+		profile_phone.setText(userContents[1]);
+		facebook_url.setText(urlContents[0]);
+		twitter_url.setText(urlContents[1]);
+		google_url.setText(urlContents[2]);
 
 		
 		return view;
@@ -50,10 +57,33 @@ public class ProfileFragment extends Fragment implements ConsumeCursor{
 
 	@Override
 	public void consumeCursor(Cursor cursor) {
-		for (int i = 0; i < cursor.getColumnCount(); i++) {
-			cursor.getColumnName(i);
+		if (!cursor.moveToFirst()) {
+			Toast.makeText(getActivity(), "The database is empty.", Toast.LENGTH_SHORT).show();
+			
+		} else if(cursor.getColumnName(0).equalsIgnoreCase("user")) {
+			cursor.moveToFirst();
+			for (int j = 0; j <= cursor.getCount(); j++) {
+				for (int i = 0; i < cursor.getColumnCount(); i++) {
+					userContents[i] = cursor.getString(i);
+				}
+				cursor.moveToPosition(j);
+			}	
+		} else if (cursor.getColumnName(0).equalsIgnoreCase("type")) {
+			cursor.moveToFirst();
+			for(int j = 0; j <= cursor.getCount(); j++) {
+				if(cursor.getString(0).equalsIgnoreCase("facebook")) {
+					urlContents[0] = cursor.getString(1);
+				} else if(cursor.getString(0).equalsIgnoreCase("twitter")) {
+					urlContents[1] = cursor.getString(1);
+				} else if (cursor.getString(0).equalsIgnoreCase("google")) {
+					urlContents[2] = cursor.getString(1);
+				}
+				cursor.moveToPosition(j);
+			}
+		} else {
+			Toast.makeText(getActivity(), "There was an issue.", Toast.LENGTH_SHORT).show();
 		}
-		
+
 	}
 	
 }
